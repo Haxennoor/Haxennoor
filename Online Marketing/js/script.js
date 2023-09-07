92adb9c1abf6ca3d600c9ef01b33ea561733df4d
Haxennoor <144370967+Haxennoor@users.noreply.github.com>
2023-09-07 21:29:23 +0500

    Add files via upload

---
+++ Online Marketing/js/script.js
@@ -0,0 +1,43 @@
+var typed=new typed(".text",{
+    stiring:["Frontend Developer" , "SEO Expert" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
